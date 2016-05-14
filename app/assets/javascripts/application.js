af3a0994c16849b6eb44e8a8f1e440443edd984a
Vicky L <vicky.sy.loo@gmail.com>
2016-05-14 15:00:51 -0400

    individual marker plots now working with cocoon specific div wrap in walks.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require cocoon
-//= require underscore
 //= require_tree .
--- app/assets/javascripts/walks.js
+++ app/assets/javascripts/walks.js
@@ -12,12 +12,17 @@
 		}
 		map = new google.maps.Map(document.getElementById('map_walk'), mapOptions);	// create new map div
 	}
-
-  	$(".waypoint-btn").click(function(event){
-      event.preventDefault();
-
+$('#mkpts').on('cocoon:after-insert', function(e){
+  // attempting to see if cocoon is effecting jquery order
+  console.log("waypoint div selected");
+  $("p#plot").on('click', function(event){
+    //   event.preventDefault();
+    console.log("button.waypoint-btn jquery event registered");
 //function to geocode adddress and plot it on map
       function codeAddress(address) {
+
+        console.log("function codeAddress is running");
+
         geocoder.geocode( { 'address': address}, function(results, status) {
           if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);			// center the map on address
@@ -26,13 +31,15 @@
               position: results[0].geometry.location
             });
           } else {
-            alert('somethign went wrong... Geocode was not successful');
+            alert('something went wrong... Geocode was not successful');
           }
         });
+        return false;
       }
       var address = $(this).closest(".nested-fields").find(".address").val();	// grab the address from the input field
+
       codeAddress(address);		// pass in the address into codeAddress function
   		});
-
+});
 	google.maps.event.addDomListener(window, 'load', initialize);		// execute init map function on page load
 	});

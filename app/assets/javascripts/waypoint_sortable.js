75b2512c91c72f385dffa718558d0bc13aae6966
Vicky L <vicky.sy.loo@gmail.com>
2016-05-26 17:05:47 -0400

    sortable jquery ui works

--- app/assets/javascripts/waypoint_sortable.js
+++ app/assets/javascripts/waypoint_sortable.js
@@ -1,10 +1,10 @@
-// $(document).on('ready page:load', function(){
-//   $('#mkpts').on('cocoon:after-insert', function() {
-//     $(function() {
-//       console.log("loading sortable")
-//       $( "#sortable" ).sortable();
-//       $( "#sortable" ).sortable('serialize');
-//       $( "#sortable" ).disableSelection();
-//     });
-//   });
-// });
+$(document).on('ready page:load', function(){
+  $('#mkpts').on('cocoon:after-insert', function() {
+    $(function() {
+      console.log("loading sortable")
+      $( "#sortable" ).sortable();
+      $( "#sortable" ).sortable('serialize');
+      $( "#sortable" ).disableSelection();
+    });
+  });
+});

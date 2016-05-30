899d6761a7f05b0f3c35deea826841a28db87d04
Vicky L <vicky.sy.loo@gmail.com>
2016-05-30 17:58:13 -0400

    changed google marker

--- app/assets/javascripts/code_address.js
+++ app/assets/javascripts/code_address.js
@@ -5,9 +5,11 @@ function codeAddress(geocoder, address, mapSelected) {
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       mapSelected.setCenter(results[0].geometry.location);// center the map on address
+      var image = 'http://s33.postimg.org/3onpkiuen/square_marker_small.png'
       var marker = new google.maps.Marker({// place a marker on the map at the address
         map: mapSelected,
-        position: results[0].geometry.location
+        position: results[0].geometry.location,
+        icon: image
       });
     } else {
       alert('something went wrong... Geocode was not successful');

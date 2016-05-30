7fee0cac6295df91875c50131c58fdc2c63c07b6
Tina Truong <tina.truong21@gmail.com>
2016-05-30 15:13:01 -0400

    fixed index spacing

--- app/assets/javascripts/walks.js
+++ app/assets/javascripts/walks.js
@@ -6,4 +6,20 @@ $( ".walkcard" ).hover(
     $( this ).find('.walkcard_desc').fadeOut('slow');
   });
 
+  /** create walks **/
+
+  $('.create_form_step_two').hide();
+  $('.create_form_step_three').hide();
+
+$( ".next_first_btn" ).on('click',function (){
+  $('.create_form_step_one').hide();
+  $('.create_form_step_one').show();
+});
+
+$( ".next_second_btn" ).on('click',function (){
+  $('.create_form_step_one').hide();
+  $('.create_form_step_one').show();
+});
+
+
 });

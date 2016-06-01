ed039408c92825ff5143d4774e070c97de502361
mokutsu <misato.okutsu@gmail.com>
2016-06-01 14:39:39 -0400

    plot path and add points again fixed

--- app/assets/javascripts/walk_form.js
+++ app/assets/javascripts/walk_form.js
@@ -30,10 +30,10 @@ $(document).on('ready page:load', function() {
 
 
     $('button.plot_walk').on('click', function(e){
-      if ($('input.address').val() == undefined || $('input.address').length) {
+      if ($('input.address').val() == undefined || $('input.address').val() == '' || $('input.address').length < 2 ) {
+        alert('Need at least 2 points to plot a path');
         return;
       }
-
       console.log('button click!');
       renderMap;
 
@@ -43,18 +43,19 @@ $(document).on('ready page:load', function() {
       });
         console.log(waypoints);
 
-          $.ajax({
-            dataType: 'json',
-            url: '/walks/new',
-            method: 'GET',
-            data: {waypoints: waypoints},
-            success: function(return_data){
-              console.log('returned json file: ' + return_data);
-              // plot_waypoints_array(return_data, map_show);
-              // initPathMap(return_data, map_show);
-              initPathMap(return_data, map_new);
-            }
-        });
+      $.ajax({
+        dataType: 'json',
+        url: '/walks/new',
+        method: 'GET',
+        data: {waypoints: waypoints},
+        success: function(return_data){
+          console.log('returned json file: ' + return_data);
+          // plot_waypoints_array(return_data, map_show);
+          // initPathMap(return_data, map_show);
+          plot_waypoints_array(return_data, map_new); 
+          initPathMap(return_data, map_new);
+        }
+      });
     });
 
     // on clicking 'submit', query for all waypoint partial forms (input.order hidden field), insert index value into hidden field, trigger submit on the form
@@ -82,10 +83,10 @@ $(document).on('ready page:load', function() {
       });
 
       $(".plot_btn").on('click', function(event){
-           // console.log("button.waypoint-btn jquery event registered");
-           var address = $(this).closest(".nested-fields").find(".address").val();
-           codeAddress(geocoder, address, map_new);
-         });
+         // console.log("button.waypoint-btn jquery event registered");
+         var address = $(this).closest(".nested-fields").find(".address").val();
+         codeAddress(geocoder, address, map_new);
+       });
     });
   }
 });

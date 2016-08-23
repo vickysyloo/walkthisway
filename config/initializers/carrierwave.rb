cadb656b2ce5d4d43c30ab1fbe9738036af759b5
dinophile <cheyenne.arrowsmith@gmail.com>
2016-08-22 20:30:43 -0400

    storage config for carrierwave updated

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,24 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider                         => 'Google',
-      :google_storage_access_key_id     => Figaro.env.google_storage_access_key,
-      :google_storage_secret_access_key => Figaro.env.google_storage_secret_key
-      }
+  #Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
 
-      config.fog_directory = 'wtw'
-      
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => 'Figaro.env.aws_storage_access_key',            # required
+    :aws_secret_access_key  => 'Figaro.env.aws_storage_secret_key',     # required
+    :region                 => 'us-west-1'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'wtw-vtcm'  
+
 end

68d6de48e9bd19089c2ecf724073d2fcd4551c01
dinophile <cheyenne.arrowsmith@gmail.com>
2016-08-26 11:33:34 -0400

    updated carrierwave initializer and connected finally to AWS in both development and production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,23 +1,9 @@
 CarrierWave.configure do |config|
-  #Use local storage if in development or test
-  if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :file
-    end
-  end
-
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-  end
 
   config.fog_credentials = {
-    :provider               => 'AWS',                             # required
-    :aws_access_key_id      => 'Figaro.env.aws_storage_access_key',            # required
-    :aws_secret_access_key  => 'Figaro.env.aws_storage_secret_key',     # required
-    :region                 => 'us-west-1'                        # optional, defaults to 'us-east-1'
+    :provider               => 'AWS',                            
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
   }
   config.fog_directory  = 'wtw-vtcm'  
 
